f828b0d90a14ecdbbb1c0231a0680b50473b1ff2
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-15 05:04:21 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+pigeon_hash = {}
+
+data.each do |categories, subcategories| 
+  subcategories.each do |key, name|
+    name.each do |pigeon_name|
+      pigeon_hash[pigeon_name] ||= {}
+      pigeon_hash[pigeon_name][categories] ||= []
+      pigeon_hash[pigeon_name][categories] << key.to_s
+      end
+    end
+  end 
+pigeon_hash
 end
