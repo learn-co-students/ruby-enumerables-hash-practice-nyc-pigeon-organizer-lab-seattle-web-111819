7772c7cbe45806844f8e5bc6e36e92b3b2c31d5f
Elias Tooloee <elitooloee@gmail.com>
2019-11-15 08:17:16 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,11 +8,11 @@ def nyc_pigeon_organizer(data)
         if !pigeons[name]
           pigeons[name]={}
         end
-  #We then assign the keys to an array for the pigeon.    
+  #We then assign the color, gender, and lives keys to arrays for the pigeon.    
         if !pigeons[name][key]
           pigeons[name][key]=[]
         end
-  #We then use .to_s to set newval to a string and add it to the end of the hash for the pigeon.      
+  #We then use .to_s to set newval to a string and add it to the hash for the pigeon. There is nothing specific to any data set in this method, so it should work for any hash that is strucured the same way.     
         pigeons[name][key]<<newval.to_s
       end
     end
